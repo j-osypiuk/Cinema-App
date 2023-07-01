28ba653022b864126762c9fa283ce494be9bf1c6
j-osypiuk <kuba6535@gmail.com>
2023-07-01 16:08:06 +0200

    Create Genre controller and views
    Done:
    - Actions for CRUD operations and views for them
    - Import bootstrap icon reference

--- CinemaApp.Models/DomainModels/Genre.cs
+++ CinemaApp.Models/DomainModels/Genre.cs
@@ -1,9 +1,13 @@
-﻿namespace CinemaApp.Models.DomainModels
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace CinemaApp.Models.DomainModels
 {
 	public class Genre
 	{
         public int Id { get; set; }
 		public string Name { get; set; }
+
+		[ValidateNever]
         public ICollection<MovieGenre> MovieGenres { get; set; }
     }
 }
---
+++ CinemaApp.Web/Controllers/GenreController.cs
@@ -0,0 +1,83 @@
+﻿using CinemaApp.DataAccess.Data;
+using CinemaApp.Models.DomainModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Web.Controllers
+{
+	public class GenreController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+
+        public GenreController(ApplicationDbContext db)
+        {
+			_db = db;
+        }
+
+        public IActionResult Index()
+		{	
+			var genres = _db.Genres.ToList();
+			return View(genres);
+		}
+
+		public IActionResult Create() 
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateAsync(Genre genre)
+		{
+			if(ModelState.IsValid)
+			{
+				_db.Genres.Add(genre);
+				await _db.SaveChangesAsync();
+
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public IActionResult Edit(int? id)
+		{	
+			if(id == null || id == 0)
+				return NotFound();
+
+			var genre = _db.Genres.Find(id);
+
+			if(genre == null) 
+				return NotFound();
+
+			return View(genre);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> EditAsync(Genre genre)
+		{
+			if(ModelState.IsValid)
+			{
+				_db.Genres.Update(genre);
+				await _db.SaveChangesAsync();
+
+				return RedirectToAction("Index");
+			}
+			return View(genre);
+		}
+
+		[ActionName("Delete")]
+		public async Task<IActionResult> DeleteAsync(int? id)
+		{
+			if (id == null || id == 0)
+				return NotFound();
+
+			var genre = _db.Genres.Find(id);
+
+			if(genre == null)
+				return NotFound();
+
+			_db.Genres.Remove(genre);
+			await _db.SaveChangesAsync();
+
+			return RedirectToAction("Index");
+		}
+	}
+}
