281889cb4bf094eec4bf60438df142ceb03a53d7
j-osypiuk <kuba6535@gmail.com>
2023-07-05 17:01:53 +0200

    Configure relationships between models and create initial migraion

--- CinemaApp.DataAccess/Data/ApplicationDbContext.cs
+++ CinemaApp.DataAccess/Data/ApplicationDbContext.cs
@@ -5,16 +5,61 @@ namespace CinemaApp.DataAccess.Data
 {
 	public class ApplicationDbContext : DbContext
 	{
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<Genre> Genres { get; set; }
-        public DbSet<MovieGenre> MovieGenres { get; set; }
+		public DbSet<Movie> Movies { get; set; }
+		public DbSet<Genre> Genres { get; set; }
+		public DbSet<MovieGenre> MovieGenres { get; set; }
+		public DbSet<Screening> Screenings { get; set; }
+		public DbSet<Room> Rooms { get; set; }
+		public DbSet<Seat> Seats { get; set; }
+		public DbSet<SeatReservation> seatReservations { get; set; }
+		public DbSet<Ticket> Tickets { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
-			modelBuilder.Entity<MovieGenre>()
-				.HasKey(x => new {x.MovieId, x.GenreId});
+			modelBuilder.Entity<Movie>(eb =>
+			{
+				eb.Property(x => x.Duration).IsRequired();
+				eb.Property(x => x.TicketPrice).IsRequired();
+				eb.HasMany(x => x.Screenings).WithOne(x => x.Movie).HasForeignKey(x => x.MovieId);
+			});
+
+			modelBuilder.Entity<Room>(eb =>
+			{
+				eb.Property(x => x.number).IsRequired();
+				eb.HasIndex(x => x.number).IsUnique();
+				eb.HasMany(x => x.Seats).WithOne(x => x.Room).HasForeignKey(x => x.RoomId);
+				eb.HasMany(x => x.Screenings).WithOne(x => x.Room).HasForeignKey(x => x.RoomId);
+			});
+
+			modelBuilder.Entity<MovieGenre>(eb =>
+			{
+				eb.HasKey(x => new { x.MovieId, x.GenreId });
+			});
+
+			modelBuilder.Entity<Screening>(eb =>
+			{
+				eb.Property(x => x.Is3D).IsRequired();
+				eb.HasMany(x => x.Tickets).WithOne(x => x.Screening).HasForeignKey(x => x.ScreeningId);
+			});
+
+			modelBuilder.Entity<Ticket>(eb =>
+			{
+				eb.HasOne(x => x.SeatReservation)
+				.WithOne(x => x.Ticket)
+				.HasForeignKey<SeatReservation>(x => x.TicketId)
+				.OnDelete(DeleteBehavior.NoAction);
+			});
+
+			modelBuilder.Entity<Seat>(eb =>
+			{
+				eb.HasKey(x => new { x.row, x.number });
+				eb.HasMany(x => x.SeatReservations)
+				.WithOne(x => x.Seat)
+				.HasForeignKey(x => new { x.row, x.number })
+				.OnDelete(DeleteBehavior.NoAction);
+			});
 		}
 	}
 }
---
+++ CinemaApp.DataAccess/Migrations/20230705145843_InitialMigration.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaApp.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Duration = table.Column<double>(type: "float", nullable: false),
+                    MainActor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Director = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TicketPrice = table.Column<double>(type: "float", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    number = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MovieGenres",
+                columns: table => new
+                {
+                    GenreId = table.Column<int>(type: "int", nullable: false),
+                    MovieId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieGenres", x => new { x.MovieId, x.GenreId });
+                    table.ForeignKey(
+                        name: "FK_MovieGenres_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MovieGenres_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Screenings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Is3D = table.Column<bool>(type: "bit", nullable: false),
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Screenings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Screenings_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Screenings_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seats",
+                columns: table => new
+                {
+                    row = table.Column<int>(type: "int", nullable: false),
+                    number = table.Column<int>(type: "int", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seats", x => new { x.row, x.number });
+                    table.ForeignKey(
+                        name: "FK_Seats_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ScreeningId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Screenings_ScreeningId",
+                        column: x => x.ScreeningId,
+                        principalTable: "Screenings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "seatReservations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ScreeningId = table.Column<int>(type: "int", nullable: false),
+                    row = table.Column<int>(type: "int", nullable: false),
+                    number = table.Column<int>(type: "int", nullable: false),
+                    TicketId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_seatReservations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_seatReservations_Screenings_ScreeningId",
+                        column: x => x.ScreeningId,
+                        principalTable: "Screenings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_seatReservations_Seats_row_number",
+                        columns: x => new { x.row, x.number },
+                        principalTable: "Seats",
+                        principalColumns: new[] { "row", "number" });
+                    table.ForeignKey(
+                        name: "FK_seatReservations_Tickets_TicketId",
+                        column: x => x.TicketId,
+                        principalTable: "Tickets",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieGenres_GenreId",
+                table: "MovieGenres",
+                column: "GenreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_number",
+                table: "Rooms",
+                column: "number",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Screenings_MovieId",
+                table: "Screenings",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Screenings_RoomId",
+                table: "Screenings",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_seatReservations_row_number",
+                table: "seatReservations",
+                columns: new[] { "row", "number" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_seatReservations_ScreeningId",
+                table: "seatReservations",
+                column: "ScreeningId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_seatReservations_TicketId",
+                table: "seatReservations",
+                column: "TicketId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seats_RoomId",
+                table: "Seats",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_ScreeningId",
+                table: "Tickets",
+                column: "ScreeningId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovieGenres");
+
+            migrationBuilder.DropTable(
+                name: "seatReservations");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropTable(
+                name: "Seats");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Screenings");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
