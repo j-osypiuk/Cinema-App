fda5ae9a187f43a8409030bc932b56639b522a2b
j-osypiuk <kuba6535@gmail.com>
2023-07-05 17:02:44 +0200

    Create Screening view model

---
+++ CinemaApp.Models/ViewModels/ScreeningVM.cs
@@ -0,0 +1,19 @@
+﻿using CinemaApp.Models.DomainModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+
+namespace CinemaApp.Models.ViewModels
+{
+	public class ScreeningVM
+	{
+		public Screening Screening { get; set; }
+
+		[DisplayName("Movie")]
+		public int SelectedMovieId { get; set; }
+        public IEnumerable<SelectListItem>? MovieSelectList { get; set; }
+
+		[DisplayName("Room")]
+		public int SelectedRoomId { get; set; }
+        public IEnumerable<SelectListItem>? RoomSelectList { get; set; }
+    }
+}
