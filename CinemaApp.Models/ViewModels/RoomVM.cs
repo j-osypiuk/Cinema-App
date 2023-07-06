00c34c83556f1d834a1630c73b01eaff222aecb2
j-osypiuk <kuba6535@gmail.com>
2023-07-06 16:01:36 +0200

    Add Room view model class

---
+++ CinemaApp.Models/ViewModels/RoomVM.cs
@@ -0,0 +1,10 @@
+﻿using CinemaApp.Models.DomainModels;
+
+namespace CinemaApp.Models.ViewModels
+{
+	public class RoomVM
+	{
+        public Room	Room { get; set; }
+        public ICollection<DateTime> ScreeningDates { get; set; }
+    }
+}
