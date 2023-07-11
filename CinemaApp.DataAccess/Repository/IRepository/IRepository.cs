8fb46ec4b007ddb4e1ee81d6364ae3c7976379b1
j-osypiuk <kuba6535@gmail.com>
2023-07-11 23:13:45 +0200

    Implementation of base repository interface and its implementation

---
+++ CinemaApp.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Bson;
+using System.Linq.Expressions;
+
+namespace CinemaApp.DataAccess.Repository.IRepository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity?> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> FindAllAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity?> FindByAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+    }
+}
---
+++ CinemaApp.DataAccess/Repository/Repository.cs
@@ -0,0 +1,48 @@
+﻿using CinemaApp.DataAccess.Data;
+using CinemaApp.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CinemaApp.DataAccess.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected ApplicationDbContext _db { get; }
+        internal DbSet<TEntity> _dbSet { get; }
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            _dbSet = _db.Set<TEntity>();
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _db.Set<TEntity>().AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> FindAllAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);   
+        }
+
+        public async Task<TEntity?> FindByAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).FirstOrDefaultAsync();
+        }
+    }
+}
