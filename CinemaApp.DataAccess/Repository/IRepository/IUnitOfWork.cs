b55c597ea15dadb4d7586c4b39ea01f7e25fb75c
j-osypiuk <kuba6535@gmail.com>
2023-07-12 00:16:57 +0200

    Create IUnitOfWork interface and its implementation

---
+++ CinemaApp.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace CinemaApp.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        IGenreRepository Genre { get; }
+        Task SaveAsync();
+    }
+}
---
+++ CinemaApp.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using CinemaApp.DataAccess.Data;
+using CinemaApp.DataAccess.Repository.IRepository;
+
+namespace CinemaApp.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public IGenreRepository Genre { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {   
+            _db = db;
+            Genre = new GenreRepository(db);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
